d00958ecfb1dbf12a6966445d584cde79378c124
Family <Family@192.168.2.3>
2013-10-03 18:19:05 -0400

    Commit for Paired Programming Assignment week 2

--- src/citmemoryjj/CITMemoryJJ.java
+++ src/citmemoryjj/CITMemoryJJ.java
@@ -4,17 +4,33 @@
  */
 package citmemoryjj;
 
+import java.util.Scanner;
 /**
  *
  * @author Family
  */
 public class CITMemoryJJ {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    String name;
+    String instructions = "Welcome to Memory! \n"
+            + "Flip a card and try and match the picture on it to another card. \n"
+            + "Keep playing and try and beat your best time. \n"
+            + "Good luck!";
+   
     public static void main(String[] args) {
-        //I was here
-        // TODO code application logic here
+        CITMemoryJJ myGame = new CITMemoryJJ();
+        myGame.getName();
+        myGame.displayHelp();
+    }
+    
+    public void getName() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+    }
+    
+    public void displayHelp() {
+        System.out.println("\nWelcome " + this.name + "\n");
+        System.out.println(this.instructions);
     }
 }
