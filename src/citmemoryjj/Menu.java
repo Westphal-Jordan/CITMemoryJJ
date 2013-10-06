b8d232e16203f7cf02b4db8eb7d0574a9857207b
Family <Family@192.168.2.3>
2013-10-05 20:04:33 -0400

    Commit for Paired Programming Assignment week 2

--- src/citmemoryjj/Menu.java
+++ src/citmemoryjj/Menu.java
@@ -6,11 +6,12 @@ public class Menu {
     String options = "MAIN MENU\n"
             + "- Begin Game         (press b)\n"
             + "- See Rules          (press r)\n"
-            + "- Change Preferences (press p)";
+            + "- Change Preferences (press p)\n"
+            + "\n";
             
     
     public void displayMenu() {
-    
+        System.out.println(options);
     
     }
 }
